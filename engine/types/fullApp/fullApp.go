12e84677276c1cd7c515316a743a694be20c3361
asmsh <8120013+asmsh@users.noreply.github.com>
2019-06-05 00:25:53 +0200

    feat: adding the ability to format the app into JSON string

--- engine/types/fullApp/fullApp.go
+++ engine/types/fullApp/fullApp.go
@@ -1,26 +1,23 @@
 package fullApp
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 // the base part of the url for an app page
 const appPageBaseUrl = "https://play.google.com/store/apps/details?id="
 
-// the attributes that are written in uppercase letters will be exported to the JSON
+// the attributes that are written in uppercase letters will be exported to the ToJSON
 type FullApp struct {
 	// this is the id of this app, i.e. the apk name
 	AppID string `json:",omitempty"`
-	// the link of this app on the store
 	AppUrl string `json:",omitempty"`
-	// the urls for the icon image of this app.
-	// image at the first index is the default(low res) image, and image at the second index is a higher res one.
+	// image url at the first index is the default(low res) image, and image at the second index is a higher res one.
 	IconUrls []string `json:",omitempty"`
-	// the name of the app
 	AppName string `json:",omitempty"`
-	// the name of the developer of this app
 	DevName string `json:",omitempty"`
-	// the url of the developer's page
 	DevPageUrl string `json:",omitempty"`
-	// the category of this app
 	Category string `json:",omitempty"`
 
 	// TODO, handle the sec category(additional family category) extraction.
@@ -34,17 +31,15 @@ type FullApp struct {
 	//ContainsAds          bool `json:"containsAds"`
 	//OffersInAppPurchases bool `json:"offersInAppPurchases"`
 
-	// the price of this app
 	Price string `json:",omitempty"`
 	// an array that contains the urls of the app's video trailer(if it provides a video trailer).
-	// the trailer's start image's url is at its first position, and the trailer's url is at its second position.
+	// the trailer's start image's url is at its first position, and the trailer's video's url is at its second position.
 	VideoTrailerUrls []string `json:",omitempty"`
 	// an array that contains the urls of the screenshots of the app, the array is populated pair by pair,
 	// the first element in every pair is the lower resolution screenshot, while the second element is the high resolution one.
-	// by that populating pattern, every odd index(index starts at 0) will be a lower resolution one,
-	// and every even index will be a high resolution one.
+	// by that populating pattern, every url at even index(index starts at 0) will be a lower resolution one,
+	// and every url at odd index will be a high resolution one.
 	ScreenShotsUrls []string `json:",omitempty"`
-	// the description of this app
 	Description string `json:",omitempty"`
 	// the rating of this app got, the aggregate rating
 	Rating string `json:",omitempty"`
@@ -55,9 +50,10 @@ type FullApp struct {
 	RatingHistogram map[string]string `json:",omitempty"`
 	// this holds the what's new section of the app page
 	WhatsNew string `json:",omitempty"`
+
+	// TODO
 	// the date of the last update for this app
 	LastUpdated string `json:",omitempty"`
-	// the size of the app (this requires some edited browser agent)
 	Size string `json:",omitempty"`
 	// the number of downloads for this app
 	NumOfDownloads string `json:",omitempty"`
@@ -69,17 +65,15 @@ type FullApp struct {
 	AgeRating string `json:",omitempty"`
 }
 
-// sets the name of the apk of this app,
 func (app *FullApp) SetAppID(appID string) {
 	app.AppID = appID
 }
 
-// sets the app page url to the given url
 func (app *FullApp) SetAppUrl(appURL string) {
 	app.AppUrl = appURL
 }
 
-// sets the app page url to the default one(which one play store choose)
+// sets the app page url to the default one(the one play store choose)
 func (app *FullApp) SetDefaultAppUrl() error {
 	if app.AppID == "" {
 		return fmt.Errorf("the app id isn't set to a value yet")
@@ -88,12 +82,10 @@ func (app *FullApp) SetDefaultAppUrl() error {
 	return nil
 }
 
-// sets the url of this app's icon image source
 func (app *FullApp) SetIconUrls(iconURLs []string) {
 	app.IconUrls = iconURLs
 }
 
-// sets the name of the app
 func (app *FullApp) SetAppName(appName string) {
 	app.AppName = appName
 }
@@ -102,12 +94,10 @@ func (app *FullApp) SetDevPageUrl(url string) {
 	app.DevPageUrl = url
 }
 
-// sets the name of the developer of this app
 func (app *FullApp) SetDevName(devName string) {
 	app.DevName = devName
 }
 
-// sets the category that this app belongs to
 func (app *FullApp) SetCategory(cat string) {
 	app.Category = cat
 }
@@ -132,7 +122,7 @@ func (app *FullApp) SetOffersInAppPurchases() {
 	app.OffersInAppPurchases = true
 }
 */
-// sets the price of this app
+
 func (app *FullApp) SetAppPrice(price string) {
 	app.Price = price
 }
@@ -141,62 +131,60 @@ func (app *FullApp) SetVideoTrailerUrls(urls []string) {
 	app.VideoTrailerUrls = urls
 }
 
-// sets the urls of the screenshots for an app
 func (app *FullApp) SetScreenShotsUrls(urls []string) {
 	app.ScreenShotsUrls = urls
 }
 
-// sets the app's description
 func (app *FullApp) SetDescription(desc string) {
 	app.Description = desc
 }
 
-// sets the rating that this app got from the user on the store
 func (app *FullApp) SetRating(rating string) {
 	app.Rating = rating
 }
 
-// sets the number of users that rated this app on the store
 func (app *FullApp) SetRatingCount(ratingCount string) {
 	app.RatingCount = ratingCount
 }
 
-// sets the rating histogram for this app
 func (app *FullApp) SetRatingHistogram(histogram map[string]string) {
 	app.RatingHistogram = histogram
 }
 
-// sets the 'whats's new' section
 func (app *FullApp) SetWhatsNew(wsNew string) {
 	app.WhatsNew = wsNew
 }
 
-// sets the date of the last update
 func (app *FullApp) SetLastUpdated(updated string) {
 	app.LastUpdated = updated
 }
 
-// sets the size of the app
 func (app *FullApp) SetAppSize(appSize string) {
 	app.Size = appSize
 }
 
-// sets the number of downloads for this app
 func (app *FullApp) SetNumOfDownloads(numOfDls string) {
 	app.NumOfDownloads = numOfDls
 }
 
-// sets the app version
 func (app *FullApp) SetAppVer(appVer string) {
 	app.AppVer = appVer
 }
 
-// sets the minimum Android version required by the app to run (install)
 func (app *FullApp) SetMinAndroidVer(minVer string) {
 	app.MinAndroidVer = minVer
 }
 
-// sets the minimum age (should be) required by this app
 func (app *FullApp) SetMinAgeRating(minAge string) {
 	app.AgeRating = minAge
+}
+
+// ToJSON returns the app info as a JSON string
+func (app *FullApp) ToJSON() (string, error) {
+	ba, e := json.Marshal(app)
+	if e != nil {
+		return "", e
+	}
+
+	return string(ba), nil
 }
